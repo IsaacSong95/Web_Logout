3413c6fd2f3ffff08e840ae56cd653c96b9ec0f0
Isaac song <80498558+kamikaze321@users.noreply.github.com>
2021-09-02 22:22:34 +1200

    Add project files.

---
+++ LoginLogout/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginLogout.Models;
+
+namespace LoginLogout.Controllers
+{
+    public class HomeController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.TBLUserInfoes.ToList());
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(TBLUserInfo tBLUserInfo)
+        {
+            if(db.TBLUserInfoes.Any(X=>X.UsernameUs == tBLUserInfo.UsernameUs))
+            {
+                ViewBag.Notification = "This account has already existed";
+                return View();
+            }
+            else
+            {
+                db.TBLUserInfoes.Add(tBLUserInfo);
+                db.SaveChanges();
+
+                Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            
+            
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(TBLUserInfo tBLUserInfo)
+        {
+            var checklogin = db.TBLUserInfoes.Where(x => x.UsernameUs.Equals(tBLUserInfo.UsernameUs) && x.PasswrodUs.Equals(tBLUserInfo.PasswrodUs)).FirstOrDefault();
+            if (checklogin !=null)
+            {
+                Session["IdUsSS"] = tBLUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tBLUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Wrong Username or password";
+            }
+        }
+
+    }
+}
